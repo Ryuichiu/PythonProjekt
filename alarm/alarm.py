f2fdfcd57900660bd23587cebe843a718bde4c85
Tschi02 <T.Schindler02@gmail.com>
2019-05-01 23:30:56 +0200

    initial commit

---
+++ alarm/alarm.py
@@ -0,0 +1,51 @@
+import RPi.GPIO as GPIO
+import time
+import threading
+
+
+class Alarm:
+    # variables
+
+    MOTION_SENSOR_PIN = 16
+    LED_PIN = 11
+
+    alarmIsOn = False
+
+    GPIO.setmode(GPIO.BOARD)
+
+    # led
+    GPIO.setup(11, GPIO.OUT)
+    GPIO.output(11, GPIO.LOW)
+
+    # motion sensor
+    GPIO.setup(MOTION_SENSOR_PIN, GPIO.IN)
+
+    @staticmethod
+    def start_alarm():
+        def start_loop():
+            Alarm.alarmIsOn = True
+            sleeper = 0.5
+            while Alarm.alarmIsOn:
+                GPIO.output(Alarm.LED_PIN, GPIO.HIGH)
+                time.sleep(sleeper)
+                GPIO.output(Alarm.LED_PIN, GPIO.LOW)
+                time.sleep(sleeper)
+
+        threading.Thread(target=start_loop).start()
+
+    @staticmethod
+    def stop_alarm():
+        Alarm.alarmIsOn = False
+
+    start_alarm()
+    time.sleep(10)
+    stop_alarm()
+
+    print("Stopped, waiting 3 Secs...")
+    time.sleep(3)
+    print("Starting Alarm now.")
+    start_alarm()
+    print("Started, waiting 10 Secs and stop program")
+    time.sleep(10)
+
+    GPIO.cleanup()
