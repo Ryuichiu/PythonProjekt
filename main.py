be806e71969fd2fd9efed0e054783bf0921ffc4c
Tschi02 <T.Schindler02@gmail.com>
2019-05-06 23:26:25 +0200

    refactoring and commenting

--- main.py
+++ main.py
@@ -13,23 +13,22 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
 GPIO.output(11, GPIO.LOW)
 
-# Bewegungsmelder
+# Motion sensor
 GPIO.setup(MOTION_SENSOR_PIN, GPIO.IN)
 
-# Datenbank
+# Database
 db = MySQLdb.connect(host="localhost",
                      user="mySQL",
                      passwd="password",
                      db="testDB")
+cursor = db.cursor()
 
-# Initialisierung
+# initiation
 Read = 0
 State = 0
 
-# Curser Initialisierung
-cursor = db.cursor()
 
-
+# LED blinks according to parameters value. Default is 3.
 def blink_led(times=3):
     for _ in range(times):
         GPIO.output(LED_PIN, GPIO.HIGH)
@@ -38,41 +37,45 @@ def blink_led(times=3):
         time.sleep(0.5)
 
 
-def insert_time(active):
+# writes the log into the DB when the state of the alarm-system changes
+def insert_time(is_active):
     try:
-        #sql = "insert into alarm_system (is_active) values(%s);"
-        cursor.execute("insert into alarm_system (is_active) values(1);") if active else\
-            cursor.execute("insert into alarm_system (is_active) values (0)")
+        cursor.execute("INSERT INTO alarm_system (is_active) VALUES(1);") if is_active else \
+            cursor.execute("INSERT INTO alarm_system (is_active) VALUES(0)")
         db.commit()
     except cursor.MySQLError:
         print(cursor.MySQLError)
 
 
 try:
-    print("Waiting, until PIR is in sleep mode ...")
+    print("Waiting, until PIR is in sleep mode...")
 
-    # Warten, bis Bewegungsmelder keine Bewegung erkennt, bevor die Endlosschleife gestartet wird
+    # Waiting until there is no more motion in front of the motion sensor.
     while GPIO.input(MOTION_SENSOR_PIN) != 0:
         time.sleep(0.5)
-    print("Start monitoring...")
-    insert_time(0)
-    insert_time(1)
 
+    print("Start monitoring...")
+
+    # Main continuous loop
     while True:
         Read = GPIO.input(MOTION_SENSOR_PIN)
+
+        # If alarm switches from 'active' to 'not active'
         if State == 1 and Read == 0:
             State = 0
             print("Switching alarm off...")
             insert_time(0)
             time.sleep(0.5)
+
+        # If alarm switches from 'not active' to 'active'
         elif Read == 1:
             State = 1
             print("Motion recognized, starting alarm...")
             insert_time(1)
             blink_led()
 
+# User can interrupt the loop with pressing ctrl + 'c'
 except KeyboardInterrupt:
     print("Stopping...")
-    # db.close
     GPIO.cleanup()
     db.close()
